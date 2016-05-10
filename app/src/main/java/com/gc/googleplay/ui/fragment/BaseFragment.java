a1f4d2ed58173074d0e7d843cdb2839f0ccd0f25
xiphodon <827789895@qq.com>
2016-05-10 21:47:54 +0800

    GooglePlay（自定义Application； UIUtils工具类封装； 导入PagerTab并解决相关报错； 主页面框架搭建; LoadingPage-加载中布局；）

--- app/src/main/java/com/gc/googleplay/ui/fragment/BaseFragment.java
+++ app/src/main/java/com/gc/googleplay/ui/fragment/BaseFragment.java
@@ -1,26 +1,55 @@
 package com.gc.googleplay.ui.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.gc.googleplay.ui.view.LoadingPage;
 import com.gc.googleplay.utils.UIUtils;
 
 /**
  * fragment基类
  */
 public class BaseFragment extends Fragment {
-    @Nullable
+
+    private LoadingPage mLoadingPage;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // 使用textview显示当前类的类名
+        // TextView view = new TextView(UIUtils.getContext());
+        // view.setText(getClass().getSimpleName());
+        mLoadingPage = new LoadingPage(UIUtils.getContext());
+//        {
+//
+////            @Override
+////            public View onCreateSuccessView() {
+////                // 注意:此处一定要调用BaseFragment的onCreateSuccessView, 否则栈溢出
+////                return BaseFragment.this.onCreateSuccessView();
+////            }
+////
+////            @Override
+////            public ResultState onLoad() {
+////                return BaseFragment.this.onLoad();
+////            }
+//
+//        };
 
-        TextView textView = new TextView(UIUtils.getContext());
-        textView.setText(getClass().getName());
-
-        return textView;
+        return mLoadingPage;
     }
+
+//    // 加载成功的布局, 必须由子类来实现
+//    public abstract View onCreateSuccessView();
+//
+//    // 加载网络数据, 必须由子类来实现
+//    public abstract LoadingPage.ResultState onLoad();
+
+//    // 开始加载数据
+//    public void loadData() {
+//        if (mLoadingPage != null) {
+//            mLoadingPage.loadData();
+//        }
+//    }
 }
